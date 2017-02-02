4447fd7fce6ee602fb56f4d70e1abd8c36947f39
Byron Blocker and Jerry Taylor <byron@>
2017-02-01 20:41:59 -0800

    add formulas to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,63 @@
-var add = function(number1, number2){
-return number1 + number2;
+//Addition Sectuin
+// var add = function(number1, number2){
+// return number1 + number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+// alert(add(number1, number2));
+
+//Subtraction Sectioin
+// var subtract = function(number1, number2){
+//   return number1 - number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+// alert(subtract(number1, number2));
+
+//Multiplication Section
+// var multiply = function(number1, number2){
+//   return number1 * number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+// alert(multiply(number1, number2));
+
+// Divide Section
+// var divide = function(number1, number2){
+//   return number1 / number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+// alert(divide(number1, number2));
+
+// var BMI = function(weight, height){
+//   return weight / (height * height) * 703;
+// };
+//
+// var weight = parseFloat(prompt("Enter your weight in pounds:"));
+// var height = parseFloat(prompt("Enter your height in inches:"));
+//
+// alert(BMI(weight, height));
+
+// var convert = function(temp){
+//   return (temp - 32) * 5 / 9;
+// };
+// var temp = parseFloat(prompt("How hot is it?"));
+//
+// alert(convert(temp));
+
+var convertGal = function(gallon){
+  return (gallon * 3.78541);
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var gallon = parseFloat(prompt("How many gallons do you have?"));
 
-alert(add(number1, number2));
+alert(convertGal(gallon));
